59aea55bb5e06201391bd2f9d19cbb3c9fceda2e
Narek <narek700@mail.ru>
2023-04-01 22:53:54 +0400

    create partners block

---
+++ assets/js/partners-carousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#partners').slick({
+        dots: false,
+        arrows: false,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 5000,
+        cssEase:'linear',
+        infinite: true,
+        focusOnSelect: false,
+    });
+});
