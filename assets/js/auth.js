e007b749b1f69cffb435b746fbcbf78d7e97e442
Narek <narek700@mail.ru>
2023-04-03 00:06:56 +0400

    create auth pages

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -6,11 +6,9 @@ $(document).ready(function () {
             if (!e.target.classList.contains('active')) {
                 e.target.src = '../../assets/img/eye.svg';
                 e.target.closest('.field').querySelector('input').type = 'password'
-                console.log(1);
             } else {
                 e.target.src = '../../assets/img/eye-close.svg';
                 e.target.closest('.field').querySelector('input').type = 'text'
-                console.log(2);
             }
         });
     });
